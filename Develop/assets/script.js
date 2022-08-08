431b26dd5d97eddabe82f65c98a7edd2e90202f4
Christa <christanewing47@gmail.com>
2022-08-07 20:49:30 -0400

    las commit

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -40,14 +40,14 @@ function timeTracker() {
 
 // L O C A L  S T O R A G E  S A V E  D A T A  O N  R E L O A D
 
-$("#hour-9 .description").val(localStorage.getItem("hour9"));
-$("#hour-10 .description").val(localStorage.getItem("hour10"));
-$("#hour-11 .description").val(localStorage.getItem("hour11"));
-$("#hour-12 .description").val(localStorage.getItem("hour12"));
-$("#hour-13 .description").val(localStorage.getItem("hour13"));
-$("#hour-14 .description").val(localStorage.getItem("hour14"));
-$("#hour-15 .description").val(localStorage.getItem("hour15"));
-$("#hour-16 .description").val(localStorage.getItem("hour16"));
-$("#hour-17 .description").val(localStorage.getItem("hour17"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 timeTracker();
